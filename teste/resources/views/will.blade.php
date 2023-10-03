d7ab905a2ae8e3e530e2e4903a21a780ff5820d6
willdevps <willgamer642@gmail.com>
2023-10-03 13:31:32 -0300

    cabeçalho pernabucanas

---
+++ teste/resources/views/will.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+  </head>
+  <body>
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="light"></html>
+    <title>Document</title>
+  </head>
+  <body>
+
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="light"></html>
+    <title>Document</title>
+  </head>
+  <body>
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="light"></html>
+    <title>Document</title>
+  </head>
+  <body>
+
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.3/dist/full.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <html data-theme="light"></html>
+    <title>Document</title>
+  </head>
+  <body>
+
+    <div class="flex-auto border-b-4 border-blue-700  bg-gray-200 text-center">Siga a gente nas Redes Sociais e saiba de todas as novidades</div>
+
+    <div class="flex items-center justify-between bg-yellow-300 p-6"><img class="aspecto-auto h-10 w-32" src="https://logodownload.org/wp-content/uploads/2014/07/pernambucanas-logo.png  " alt="busca" /> 
+    <input type="text" placeholder="O que você está procurando?" class="input join-item w-[3000px] max-w-screen-sm "> 
+
+   <div class="flex-row text-justify">
+  <span class="font-bold text-blue-600  ">Olá, faça seu login </span>
+  <br> <p class="font-thin text-sm"> ou cadastre-se </p>
+
+  
+</div>   
+</div>
+<div class="navbar flex-row text-base text-black bg-yellow-300"> 
+  <a class="btn btn-ghost normal-case text-xl">Todos os departamentos</a> 
+  <a class="btn btn-ghost normal-case text-xl">Feminino</a>
+   <a class="btn btn-ghost normal-case text-xl">Masculino</a>
+    <a class="btn btn-ghost normal-case text-xl">Infantil</a>
+     <a class="btn btn-ghost normal-case text-xl">Casa</a>
+      <a class="btn btn-ghost normal-case text-xl">Eletroportáteis</a>
+       <a class="btn btn-ghost normal-case text-xl">Telefonia</a>
+        <a class="btn btn-ghost normal-case text-xl">EXCLUSIVOS</a>
+         <a class="btn btn-ghost normal-case text-xl">Beleza</a>
+          <a class="btn btn-ghost normal-case text-xl">Espaço Disney </a>
+          <a class="btn btn-ghost normal-case text-xl">Voltei</a> 
+
+          
+</div>
+  <div class="bg-blue-800 p-4">  
+  </div>
+  </body>
+</html>
+
+          
+</div>
+    
+  </div>
+  </body>
+</html>
+
+  </body>
+</html>
